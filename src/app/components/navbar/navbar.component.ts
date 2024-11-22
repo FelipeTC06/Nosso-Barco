b3b4964e8bbd4367c9da2902d1da586a647558e7
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2024-11-22 14:06:19 -0300

    feat: create nav bar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { SearchBarComponent } from "../search-bar/search-bar.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, SearchBarComponent, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  faCircleUser = faCircleUser
+  screenWidth: number | null = null;
+  smallDevice: boolean = false;
+  
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      this.screenWidth = window.innerWidth;
+      if (this.screenWidth <= 620) {
+        this.smallDevice = true;
+      }
+    }
+  }
 
 }
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+  faSearch = faSearch
+  screenWidth: number | null = null;
+  smallDevice: boolean = false;
+  
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      this.screenWidth = window.innerWidth;
+      if (this.screenWidth <= 620) {
+        this.smallDevice = true;
+      }
+    }
+  }
 
 }
