622cbff50809d30e5cbedc6a5e74f33cce2a15d1
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2024-11-22 14:23:08 -0300

    feat: create galery component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchLayoutComponent } from "../../components/search-layout/search-layout.component";
+import { GaleryComponent } from "../../components/galery/galery.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchLayoutComponent],
+  imports: [SearchLayoutComponent, GaleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
