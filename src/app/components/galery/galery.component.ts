fe9b997d9e9ed1d486dfd1ff60e919f0c0e1e02d
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2024-11-23 00:27:55 -0300

    fix: create galery css

--- src/app/components/galery/galery.component.ts
+++ src/app/components/galery/galery.component.ts
@@ -8,5 +8,76 @@ import { Component } from '@angular/core';
   styleUrl: './galery.component.scss'
 })
 export class GaleryComponent {
-
+  public items = [
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+    {
+      src: 'images/fot-exemplo-lancha-1.jpg',
+      location: 'Angra dos Reis, Rj',
+      maxNumberOnBoard: '4',
+      period: '2 - 7 de dez',
+      ammout: '290,00',
+    },
+  ]
 }
